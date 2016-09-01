4ebb479332295bb90ef2f27f1b677410e8b08bab
Daniel Jastrzębski <grands.hawk@gmail.com>
2016-09-02 00:14:26 +0200

    Initial commit with sample bootstrap site

---
+++ src/main/java/com/eshop/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Admin on 01.09.2016.
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
